05f7148ff4d5a49b314c3343e93cfd14a5568eb3
Glow <vr837017@gmail.com>
2023-02-20 10:58:54 +0300

    make patch in FileLoader more flexible

--- src/main/java/fileManager/FileLoader.java
+++ src/main/java/fileManager/FileLoader.java
@@ -6,16 +6,19 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class FileLoader {
 
     private static final FileLoader fileLoader = new FileLoader();
 
+    private Path path = Path.of("src/main/resources/foodTable.xlsx");
+
     private FileLoader() {}
 
     public Sheet loadFoodTable() {
 
-        try (FileInputStream fileInputStream = new FileInputStream("D:\\Java\\LifeSimulatorNew\\src\\main\\resources\\foodTable.xlsx");
+        try (FileInputStream fileInputStream = new FileInputStream(path.toAbsolutePath().toFile());
              Workbook workbook = new XSSFWorkbook(fileInputStream)) {
 
             return workbook.getSheet("food");
